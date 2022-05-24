6a2f6019daf3d720035bfb48e72fc2e43b4a66c0
mike <michael@goodbuygear.com>
2022-05-24 07:48:56 -0700

    test run

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const path = require('path')
 const cookieParser = require('cookie-parser')
-
+require('dotenv').config()
 const messagesRoute = require('./routes/messages')
+
+
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.locals.moment = require('moment');
-require('dotenv').config()
 app.use('/', messagesRoute)
 app.set('view engine', 'pug')
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`TMWSD is listening at http://localhost:${port}`)
 })
+
+module.exports = server;
--- routes/messages.js
+++ routes/messages.js
@@ -4,31 +4,54 @@ const models = require('../models/index')
 const csurf = require('csurf')
 const csrfProtection = csurf({ cookie: true })
 
-router.get('/:id', async (req, res) => {
+router.get('/show/:id', async (req, res) => {
   try {
+    const page = req.query.page;
     const message = await models.message.findByPk(req.params.id)
     message.destroy();
-    res.render('messages/show', { message })
+    res.render('messages/show', { message, page })
   } catch(error) {
-    console.log(error.message)
+    console.log(error)
+    let message = {title: 'Error', text: 'There was an error', createdAt: new Date()}
+    res.render('messages/show', { message })
   }
 })
 
 router.get('/', csrfProtection, async (req, res)  =>  {
+  if (!req.query.page || req.query.page < 1) {
+    res.redirect('?page=1')
+  }
+  const pageSize = 10;
+  const skip = (Number(req.query.page) - 1) * pageSize;
+
   try {
-    const allMessages = await models.message.findAll()
-    res.render('messages/index', { messages: allMessages, csrfToken: req.csrfToken() })
+    let messageCount = await models.message.count();
+    if (req.query.page > Math.ceil(messageCount / pageSize)) {
+      res.redirect('?page=1')
+    }
+    if (messageCount > 0) {
+    const messages = await models.message.findAll({offset: skip, limit: pageSize, order: [['createdAt', 'DESC']]})
+    res.render('messages/index', {
+      page:Number(req.query.page),
+      messageStart: (Number(req.query.page) -1 ) * pageSize + 1,
+      messageEnd: Number(req.query.page) * pageSize > messageCount ? messageCount : Number(req.query.page) * pageSize,
+      messageCount, 
+      skip,
+      messages, 
+      csrfToken: req.csrfToken() })
+  }
   } catch(error) {
-    console.log(error.message)
+    res.render('error')
   }
 
 })
 
 router.post('/new', csrfProtection, async (req, res) => {
   try {
+    const {title, text} = req.body
     const message = await models.message.create({
-      title: req.body.title,
-      text: req.body.message,
+      title,
+      text
     })
     res.redirect('/')
   } catch(error) {
